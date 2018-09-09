68355f1ced725f83e77aba697fd0e71ecebed529
Habib Aroua <HabibAroua@users.noreply.github.com>
2018-09-09 20:42:51 +0100

    init commit

--- Magasin/Magasin/Program.cs
+++ Magasin/Magasin/Program.cs
@@ -1,6 +1,6 @@
 ﻿using System;
 using System.Windows.Forms;
-
+using Magasin.Sql_Server;
 namespace Magasin
 {
     static class Program
@@ -10,7 +10,14 @@ namespace Magasin
         {
             Application.EnableVisualStyles();
             Application.SetCompatibleTextRenderingDefault(false);
-            Application.Run(new Authentification());
+            if (Test_Connection.testConnection())
+            {
+                Application.Run(new Authentification());
+            }
+            else
+            {
+                MessageBox.Show("Le serveur est désactiveé", "Error", MessageBoxButtons.OK, MessageBoxIcon.Error);
+            }
         }
     }
 }
---
+++ Magasin/Magasin/Sql Server/Test_Connection.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Data.SqlClient;
+
+namespace Magasin.Sql_Server
+{
+    class Test_Connection
+    {
+        public static Boolean testConnection()
+        {
+            try
+            {
+                SqlConnection cn;
+                SqlCommand cmd;
+                cn = new SqlConnection("Data Source=DESKTOP-UU0N3RP\\SQLEXPRESS;Initial Catalog=Magasin_M4;Integrated Security=True"); //Source= nom de serveur , Catalog= nom de la base de donnee
+                cmd = new SqlCommand();
+                return true;
+            }
+            catch (Exception ex)
+            {
+                System.Console.WriteLine(ex.Message);
+                return false;
+            }
+        }
+    }
+}
