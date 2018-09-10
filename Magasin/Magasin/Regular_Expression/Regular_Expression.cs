550e35a4cc33a00bd2414f78db44ef3ba44fb0c3
Habib Aroua <HabibAroua@users.noreply.github.com>
2018-09-10 09:06:47 +0100

    add new Class

---
+++ Magasin/Magasin/Regular_Expression/Regular_Expression.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Magasin.Regular_Expression
+{
+    class Regular_Expression
+    {
+        public Boolean CommencerParBloc()
+        {
+            return false;
+        }
+    }
+}
