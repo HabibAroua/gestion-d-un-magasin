20b4f281fed22d84ac9aea18fd1141f724065a5b
Habib Aroua <HabibAroua@users.noreply.github.com>
2018-09-24 23:02:18 +0100

    Edit

--- Magasin/Magasin/GestionFabricant.Designer.cs
+++ Magasin/Magasin/GestionFabricant.Designer.cs
@@ -61,14 +61,14 @@
             // dataGridView1
             // 
             this.dataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
-            this.dataGridView1.Location = new System.Drawing.Point(138, 118);
+            this.dataGridView1.Location = new System.Drawing.Point(113, 118);
             this.dataGridView1.Name = "dataGridView1";
             this.dataGridView1.Size = new System.Drawing.Size(221, 177);
             this.dataGridView1.TabIndex = 25;
             // 
             // button1
             // 
-            this.button1.Location = new System.Drawing.Point(164, 89);
+            this.button1.Location = new System.Drawing.Point(144, 89);
             this.button1.Name = "button1";
             this.button1.Size = new System.Drawing.Size(75, 23);
             this.button1.TabIndex = 26;
@@ -77,7 +77,7 @@
             // 
             // button2
             // 
-            this.button2.Location = new System.Drawing.Point(256, 89);
+            this.button2.Location = new System.Drawing.Point(234, 89);
             this.button2.Name = "button2";
             this.button2.Size = new System.Drawing.Size(75, 23);
             this.button2.TabIndex = 27;
