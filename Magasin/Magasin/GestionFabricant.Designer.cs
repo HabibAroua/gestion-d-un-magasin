13246a704e59d25d69df18f033b08afd9bf30d88
Habib Aroua <HabibAroua@users.noreply.github.com>
2018-09-24 22:59:14 +0100

    Edit size

--- Magasin/Magasin/GestionFabricant.Designer.cs
+++ Magasin/Magasin/GestionFabricant.Designer.cs
@@ -48,7 +48,7 @@
             this.label9.AutoSize = true;
             this.label9.Font = new System.Drawing.Font("Microsoft Sans Serif", 18F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.label9.ForeColor = System.Drawing.SystemColors.ButtonHighlight;
-            this.label9.Location = new System.Drawing.Point(247, 29);
+            this.label9.Location = new System.Drawing.Point(221, 24);
             this.label9.Name = "label9";
             this.label9.Size = new System.Drawing.Size(239, 29);
             this.label9.TabIndex = 0;
